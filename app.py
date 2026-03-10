a0945fe1d225b11348d9c0e44604d3b27902dc26
Manvith <mjraip06@gmail.com>
2026-03-10 11:29:38 +0530

    Added resume to Flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
@@ -7,7 +7,14 @@ def home():
     return """
     <h1>Name: Manvith Rai</h1>
     <h2>AppID: APP123</h2>
+    <h3>Hobbies: Coding, Gym, Music</h3>
+    <br>
+    <a href="/resume">Download My Resume</a>
     """
+
+@app.route("/resume")
+def resume():
+    return send_file("Manvith Rai.pdf")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
