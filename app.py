bd06cf0bc622245efe77f9db095a40dff8f6f212
Manvith <mjraip06@gmail.com>
2026-03-10 11:12:44 +0530

    Inittial commit

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Manvith Rai</h1>"
+def home():
+    return """
+    <h1>Name: Manvith Rai</h1>
+    <h2>AppID: APP123</h2>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
